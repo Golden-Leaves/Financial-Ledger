1f1031bd6c6a50c2a1b00094427c83fc1a1fca4c
Golden_Leaves <letuongminh10@gmail.com>
2025-06-21 11:55:43 +0700

    add language_leap_Ai

--- jisho_audio_scraper/main.py
+++ jisho_audio_scraper/main.py
@@ -1,11 +1,7 @@
 from words import JishoAudioDownloader,hiragana_words
 import requests
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException,ElementClickInterceptedException,TimeoutException
+
 
 def main():
     jisho_audio_downloader = JishoAudioDownloader(hiragana_words)
--- jisho_audio_scraper/words.py
+++ jisho_audio_scraper/words.py
@@ -8,12 +8,31 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import requests
 import os
-hiragana_words = [
-    "さく", "すし", "い", "あい", "あおい", "かく", "こうこうせい", "かげ",
-    "かぎ", "きおく", "えいが", "うえ", "か", "か", "きく", "こえ",
-    "おおきい", "ず", "じこ", "ぞう", "おかし", "さけ", "さいご", "そうぞう"]#Avoid cluttering main and this also acts as testing data
+hiragana_words = {
+    "かさ": ["umbrella"],
+    "おす": ["to push"],
+    "きし": ["knight"],
+    "すぐ": ["immediately"],
+    "すき": ["like"],
+    "けす": ["to erase"],
+    "くじ": ["lottery"],
+    "しお": ["salt"],
+    "いしき": ["consciousness"],
+    "しぐさ": ["gesture", "mannerism"],
+    "さそう": ["to invite"],
+    "おこす": ["to wake someone up", "to cause to happen"],
+    "すこし": ["a little"],
+    "さがす": ["to search"],
+    "しかく": ["square"],
+    "かえす": ["to return"],
+    "うさぎ": ["rabbit"],
+    "さいせい": ["playback"],
+    "おかしい": ["strange"],
+    "がくせい": ["student"]
+}#Avoid cluttering main and this also acts as testing data
 class JishoAudioDownloader():
     def __init__(self,words):
+        os.makedirs("audio", exist_ok=True)
         self.words = words
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
@@ -28,17 +47,29 @@ class JishoAudioDownloader():
         for word in self.words:
             time.sleep(1.5)
             self.driver.get(f"https://jisho.org/search/{word}")
-            audio_element = self.driver.find_elements(By.CSS_SELECTOR,value="source[src$='.mp3']")[0] #only first mp3 audio for now
-            audio_link = audio_element.get_attribute("src")
-            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}          
-            parameters = {"downloadformat": "mp3"}
-            audio_file = requests.get(audio_link,params=parameters,headers=headers)
-            print(audio_file)
-            download_path = os.path.join("audio",word + ".mp3")
-            with open(download_path,"wb") as f:
-                f.write(audio_file.content)
+            word_blocks = self.driver.find_elements(By.CSS_SELECTOR,value=".concept_light.clearfix")
+            for word_block in word_blocks:
+                try:
+                    if self.words[word][0] in word_block.find_element(By.CSS_SELECTOR,".meaning-meaning").text:
+                        audio_element = word_block.find_element(By.CSS_SELECTOR,value="source[src$='.mp3']") #only first mp3 audio for now
+                        audio_link = audio_element.get_attribute("src")
+                        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            }          
+                        parameters = {"downloadformat": "mp3"}
+                        audio_file = requests.get(audio_link,params=parameters,headers=headers)
+                        audio_file.raise_for_status()
+                        print(audio_file)
+                        download_path = os.path.join("audio",word + ".mp3")
+                        with open(download_path,"wb") as f:
+                            f.write(audio_file.content)
+                        break #Stops after finding the desired audio
                     
+                    else:
+                        continue
+
+                except Exception as e:
+                    print(f"❌ Error while processing '{word}': {e}")
+                    continue
             
         
 if __name__ == "__main__":
