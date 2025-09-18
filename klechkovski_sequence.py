64d84420e14ff4aba6d0a4a1176e4f1cb864a343
Golden_Leaves <letuongminh10@gmail.com>
2025-09-18 22:39:08 +0700

    Add .gitignore

--- klechkovski_sequence.py
+++ klechkovski_sequence.py
@@ -1,17 +1,21 @@
 def klechkovski(n,l = None):
     
-    quantum_hash = {'s': 0, 'p': 1, 'd': 2, 'f': 3}
+    quantum_hash = {'s': 0, 'p': 1, 'd': 2, 'f': 3, "g":4}
     reversed_quantum_hash = {v: k for k, v in quantum_hash.items()}
     klechkovski_sequence = []
     if not l:
-        l = n - 1 #Sets l to the maximum that the max shell can handle
-    for shell in range(1,n + 1):
+        l = n - 1 #Sets l to the maximum that the max shell can handle, l starts at 0
+    for shell in range(1,(n + l) + 1):
         for subshell in range(shell):
             if subshell in reversed_quantum_hash:
                 label = f"{shell}{reversed_quantum_hash[subshell]}"
                 priority = shell + subshell
-                klechkovski_sequence.append((priority,shell,label)) #Sort by priority, then by n(if tie like 2p and 3s)
+                #If an element's TRUE power is smaller than the MAX power, then add
+                if priority <= (n + l): #n + l is the TRUE power of an element, Klechkowski sorts by that
+                    klechkovski_sequence.append((priority,shell,label)) #Sort by priority, then by n(if tie like 2p and 3s)
     klechkovski_sequence.sort()
     return klechkovski_sequence
-for orbital in klechkovski(3):
-    print(orbital[2])
+klechkovski_sequence = []
+for orbital in klechkovski(4):
+    klechkovski_sequence.append(orbital[2])
+print(" ".join(klechkovski_sequence))
